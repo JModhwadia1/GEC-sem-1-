734850fa2d85aa13c93220a238826522775cdc3f
JModhwadia1 <91890506+JModhwadia1@users.noreply.github.com>
2021-10-28 09:33:57 +0100

    added new programs from 10 -13

---
+++ Gec/Week 3/program10ShareOfHaribo.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int packets = 40;
+//	int students = 14;
+//	int myShare;
+//	int studentShare;
+//	
+//	studentShare = (packets / students);
+//	myShare = (packets % students  );
+//	cout << "student share: " << studentShare << endl;
+//	cout << "myShare: " << myShare << endl;
+//
+//	return 0;
+//
+//}
+//
---
+++ Gec/Week 3/program11Doom.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+		
+
+		string A = "I'm too young to die!";
+		string B = "Hey, not to rough!";
+		string C = "Hurt me plenty.";
+		string D = "Ultra-Violence.";
+		string E = "Nightmare";
+
+		int playerInput;
+		cin >> playerInput;
+
+		switch (playerInput)
+		{
+		case 1:
+			cout << A;
+				break;
+
+		default:
+			break;
+		}
+
+
+}
